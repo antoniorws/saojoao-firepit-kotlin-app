fce9c869b10e69414d00a80df117af132e154d01
Antonio Serra <antonio.r.serra@accenture.com>
2020-06-06 17:09:04 -0300

    adicionando propaganda

--- app/src/main/java/com/melhoresapps/fogueira/MainActivity.kt
+++ app/src/main/java/com/melhoresapps/fogueira/MainActivity.kt
@@ -3,11 +3,22 @@ package com.melhoresapps.fogueira
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var adView: AdView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adView = findViewById(R.id.adView)
+        MobileAds.initialize(this, getString(R.string.admob_appid))
+        val adRequest = AdRequest.Builder().build()
+        adView.loadAd(adRequest)
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
